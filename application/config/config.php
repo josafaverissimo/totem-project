5773b24fce6448a3aa7770bfad0d989d04bb85ed
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2022-12-04 12:58:34 -0300

    feat(language): language portuguese-br added

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/totem-project';
+$config['base_url'] = 'http://192.168.0.22:8080/totem-project';
 
 /*
 |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']    = 'english';
+$config['language']    = 'portuguese-br';
 
 /*
 |--------------------------------------------------------------------------
