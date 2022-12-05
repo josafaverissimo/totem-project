38cfd7ef1d93620bedb970724d971a1d0b262be4
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2022-12-05 12:48:41 -0300

    feat(navbar): button to go back on history

--- application/views/components/navbar.php
+++ application/views/components/navbar.php
@@ -3,6 +3,12 @@
         <li class="nav-item">
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
+        <li class="nav-item">
+            <a class="nav-link" onclick="history.back()" role="button">
+                <i class="fas fa-arrow-left"></i>
+            </a>
+        </li>
+
     </ul>
 
     <ul class="navbar-nav ml-auto">
