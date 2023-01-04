2fbb8a816bfa97739b28b196c660f60afe50b98a
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2023-01-04 11:30:34 -0300

    fix(sidebar): icons and links fixed

--- application/views/components/sidebar.php
+++ application/views/components/sidebar.php
@@ -15,45 +15,104 @@
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <li class="nav-item">
                     <a href="<?= base_url("dashboard"); ?>" class="nav-link">
-                        <i class="fas fa-home"></i>
+                        <i class="fas fa-home nav-icon"></i>
                         <p>Dashboard</p>
                     </a>
                 </li>
 
                 <li class="nav-item">
-                    <a href="<?= base_url("user"); ?>" class="nav-link">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor"
-                             class="bi bi-person-fill-gear" viewBox="0 0 20 20">
-                            <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm-9 8c0 1 1 1 1 1h5.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.544-3.393C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4Zm9.886-3.54c.18-.613 1.048-.613 1.229 0l.043.148a.64.64 0 0 0 .921.382l.136-.074c.561-.306 1.175.308.87.869l-.075.136a.64.64 0 0 0 .382.92l.149.045c.612.18.612 1.048 0 1.229l-.15.043a.64.64 0 0 0-.38.921l.074.136c.305.561-.309 1.175-.87.87l-.136-.075a.64.64 0 0 0-.92.382l-.045.149c-.18.612-1.048.612-1.229 0l-.043-.15a.64.64 0 0 0-.921-.38l-.136.074c-.561.305-1.175-.309-.87-.87l.075-.136a.64.64 0 0 0-.382-.92l-.148-.045c-.613-.18-.613-1.048 0-1.229l.148-.043a.64.64 0 0 0 .382-.921l-.074-.136c-.306-.561.308-1.175.869-.87l.136.075a.64.64 0 0 0 .92-.382l.045-.148ZM14 12.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0Z"/>
-                        </svg>
+                    <a href="#" class="nav-link">
+                        <i class="fas fa-users-cog nav-icon"></i>
                         <p>
                             Usuários
+                            <i class="fas fa-angle-left right"></i>
                         </p>
                     </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="<?= base_url("user"); ?>" class="nav-link">
+                                <i class="far fa-circle nav-icon nav-icon"></i>
+                                <p>Tabela</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="<?= base_url("user/form"); ?>" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Formulário</p>
+                            </a>
+                        </li>
+                    </ul>
                 </li>
 
                 <li class="nav-item">
-                    <a href="<?= base_url("client"); ?>" class="nav-link">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor"
-                             class="bi bi-people-fill" viewBox="0 0 20 20">
-                            <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
-                        </svg>
+                    <a href="#" class="nav-link">
+                        <i class="fas fa-users nav-icon"></i>
                         <p>
                             Clientes
+                            <i class="fas fa-angle-left right"></i>
                         </p>
                     </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="<?= base_url("client"); ?>" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Tabela</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="<?= base_url("client/form"); ?>" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Formulário</p>
+                            </a>
+                        </li>
+                    </ul>
                 </li>
 
                 <li class="nav-item">
-                    <a href="<?= base_url("event/formEvent"); ?>" class="nav-link">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor"
-                             class="bi bi-calendar-event-fill" viewBox="0 0 20 20">
-                            <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zm-3.5-7h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5z"/>
-                        </svg>
+                    <a href="#" class="nav-link">
+                        <i class="fas fa-calendar nav-icon"></i>
                         <p>
                             Eventos
+                            <i class="fas fa-angle-left right"></i>
                         </p>
                     </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="<?= base_url("event"); ?>" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Tabela</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="<?= base_url("event/form"); ?>" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Formulário</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="fas fa-circle nav-icon"></i>
+                                <p>
+                                    Categorias
+                                    <i class="fas fa-angle-left right"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="<?= base_url("event/category"); ?>" class="nav-link">
+                                        <i class="fas fa-circle nav-icon"></i>
+                                        <p>Tabela</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="<?= base_url("event/category/form"); ?>" class="nav-link">
+                                        <i class="fas fa-circle nav-icon"></i>
+                                        <p>Formulário</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
                 </li>
             </ul>
         </nav>
