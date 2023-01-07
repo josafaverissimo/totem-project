b71a53a1e46ec0cfa519fc5d16e7e334f4b60f9e
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2023-01-06 23:36:33 -0300

    feat(base_table.js): basic functions to table

---
+++ public/assets/js/base_table.js
@@ -0,0 +1,40 @@
+function deleteRow(button) {
+    const form = document.querySelector("#main-table")
+    const hash = button.dataset.hash
+    const action = button.dataset.action
+
+    form.classList.add("my-disabled")
+
+    if (hash !== "") {
+        const formData = new FormData()
+
+        formData.append("hash", hash)
+
+        fetch(action + "/" + hash, {
+            method: "post",
+            body: formData
+        }).then(function (response) {
+            return response.json()
+
+        }).then(function (json) {
+            if (json.success) {
+                mainDatatable.row($("#delete-row")).remove().draw()
+                $("#delete-modal").modal("hide")
+            } else {
+                document.getElementById("delete-row").removeAttribute("id")
+            }
+
+            if (json.messages) {
+                Object.keys(json.messages).forEach(function (status) {
+                    json.messages[status].forEach(function (message) {
+                        toastify(message, status)
+                    })
+                })
+            }
+        })
+
+        setTimeout(function () {
+            form.classList.remove("my-disabled")
+        }, 1500)
+    }
+}
