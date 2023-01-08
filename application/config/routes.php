08459169d8c7efc129aa6b3a2bffa2346cc681f0
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2023-01-07 23:14:10 -0300

    fix(routes): EventCategory create route fixed

--- application/config/routes.php
+++ application/config/routes.php
@@ -56,5 +56,6 @@ $route['translate_uri_dashes'] = FALSE;
 $route['event/category'] = "EventCategory";
 $route['event/category/form'] = "EventCategory/form";
 $route['event/category/form/(:any)'] = "EventCategory/form/$1";
+$route['event/category/create'] = "EventCategory/create";
 $route['event/category/edit/(:any)'] = "EventCategory/edit/$1";
 $route['event/category/delete/(:any)'] = "EventCategory/delete/$1";
